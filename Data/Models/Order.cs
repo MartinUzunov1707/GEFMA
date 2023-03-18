016cd9728022078cf415bcc9687ca97610bfd459
GeorgiKambarev <kambarev21@gmail.com>
2023-03-18 14:48:25 +0200

    Made Order Model

---
+++ Data/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Order
+    {
+        public Order(List<Dish> OrderDishes, float OrderPrice, float OrderETA)
+        {
+            this.OrderedDishes = OrderDishes;
+            this.Price= OrderPrice;
+            this.ETA= OrderETA;
+        }
+        public List<Dish> OrderedDishes { get; set; }
+        public  { get; set; }
+        public float ETA { get; set; }
+    }
+  
+        
+}
