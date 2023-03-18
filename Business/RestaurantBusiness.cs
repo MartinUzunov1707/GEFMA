dabf60e26a3f876dbf37f9acfca20ede0596cd0a
Kamui <Kamuicho>
2023-03-18 13:19:10 +0200

    Implemented RestaurantBusiness class.

--- Business/RestaurantBusiness.cs
+++ Business/RestaurantBusiness.cs
@@ -3,10 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data;
 
 namespace Business
 {
     public class RestaurantBusiness
     {
+        private RestaurantContext RestaurantContext;
+        public List<Dish> GetAll()
+        {
+            using (RestaurantContext = new RestaurantContext())
+            {
+                return RestaurantContext.Dishes.ToList();
+            }
+        }
+        public Dish Get(int id)
+        {
+            using (RestaurantContext = new RestaurantContext())
+            {
+                return RestaurantContext.Dishes.Find(id);
+            }
+        }
+        public void Add(Dish dish)
+        {
+            using (RestaurantContext = new RestaurantContext())
+            {
+                RestaurantContext.Dishes.Add(dish);
+                RestaurantContext.SaveChanges();
+            }
+        }
+        public void Update(Dish dish)
+        {
+            using (RestaurantContext = new RestaurantContext())
+            {
+                Dish item = RestaurantContext.Dishes.Find(dish.ID);
+                if (item != null)
+                {
+                    RestaurantContext.Entry(item).CurrentValues.SetValues(dish);
+                    RestaurantContext.SaveChanges();
+                }
+            }
+        }
+        public void Delete(int id)
+        {
+            using (RestaurantContext = new RestaurantContext())
+            {
+                Dish item = RestaurantContext.Dishes.Find(id);
+                if (item != null)
+                {
+                    RestaurantContext.Dishes.Remove(item);
+                    RestaurantContext.SaveChanges();
+                }
+            }
+        }
     }
 }
