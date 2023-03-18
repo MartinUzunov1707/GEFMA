2db1d31351ec49a2edb62679be76219bba39355a
Мартин Узунов <mu26969916@edu.mon.bg>
2023-03-18 17:35:10 +0200

    Made buttonClick function.

--- GEFMA/LoginPage.cs
+++ GEFMA/LoginPage.cs
@@ -12,6 +12,9 @@ namespace GEFMA
 {
     public partial class LoginPage : Form
     {
+        
+        private string debugUsername = "Murata123";
+        private string debugPassword = "123456789";
         public LoginPage()
         {
             InitializeComponent();
@@ -21,6 +24,11 @@ namespace GEFMA
             WindowState = FormWindowState.Normal;
             FormBorderStyle = FormBorderStyle.None;
             WindowState = FormWindowState.Maximized;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
