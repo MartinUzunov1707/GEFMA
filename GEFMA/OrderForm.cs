afdf15c3931baf09aad40d9ab1047bd856c31192
Мартин Узунов <mu26969916@edu.mon.bg>
2023-03-18 15:54:25 +0200

    Implemented a ConvertImageToByteArray and ConvertByteArrayToImage functions.

--- GEFMA/OrderForm.cs
+++ GEFMA/OrderForm.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,27 @@ namespace GEFMA
             InitializeComponent();
             currentDish = dish;
         }
-
+        public byte[] ConvertImageToByte(Image img)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+        public Image ConvertByteArrayToImage(byte[] data)
+        {
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                return Image.FromStream(ms);
+            }
+        }
         private void OrderForm_Load(object sender, EventArgs e)
         {
+            if(currentDish == null)
+            {
+                //implement Login as Admin feature.
+            }
 
         }
     }
