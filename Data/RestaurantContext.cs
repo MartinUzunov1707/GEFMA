64ed291c3d5914eefa888e3e937f82498fa18c89
Kamui <Kamuicho>
2023-03-23 16:04:39 +0200

    Added XML documentation to the RestaurantContext class.

--- Data/RestaurantContext.cs
+++ Data/RestaurantContext.cs
@@ -6,10 +6,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Data
-{  
+{
+    /// <summary>
+    /// RestaurantContext class. Inherits from DbContext. (Code first approach)
+    /// </summary>
     public class RestaurantContext : DbContext
     {
+        /// <summary>
+        /// A constructor for the class that passes the name of the context to the DbContext constructor.
+        /// </summary>
         public RestaurantContext() : base("name=RestaurantContext") { }
+        /// <summary>
+        /// A public field of the DbSet type that stores dishes.
+        /// </summary>
         public DbSet<Dish> Dishes { get; set; }
     }
  }
