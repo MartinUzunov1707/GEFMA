62a4224a66b3021b55232791a3717a43154b74d3
Мартин Узунов <mu26969916@edu.mon.bg>
2023-03-23 15:32:19 +0200

    Created tests for Order and Dish models

---
+++ UnitTests/DishModelTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Models;
+using Data;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class DishModelTests
+    {
+        [TestMethod]
+        public void DishModelKeepsNameValue()
+        {
+            string name = "TestName";
+            decimal dishPrice = 12.2m;
+            string description = "Test Description";
+            byte[] testImage = new byte[64];
+            Dish testDish = new Dish(name,dishPrice,description,testImage,true,true,true,true,15.2f);
+            Assert.AreEqual(name, testDish.Name);
+        }
+        [TestMethod]
+        public void DishModelKeepsPriceValue()
+        {
+            string name = "TestName";
+            decimal dishPrice = 12.2m;
+            string description = "Test Description";
+            byte[] testImage = new byte[64];
+            Dish testDish = new Dish(name, dishPrice, description, testImage, true, true, true, true, 15.2f);
+            Assert.AreEqual(dishPrice, testDish.Price);
+        }
+        [TestMethod]
+        public void DishModelKeepsDescriptionValue()
+        {
+            string name = "TestName";
+            decimal dishPrice = 12.2m;
+            string description = "Test Description";
+            byte[] testImage = new byte[64];
+            Dish testDish = new Dish(name, dishPrice, description, testImage, true, true, true, true, 15.2f);
+            Assert.AreEqual(description, testDish.Description);
+        }
+        [TestMethod]
+        public void DishModelKeepsImageValue()
+        {
+            string name = "TestName";
+            decimal dishPrice = 12.2m;
+            string description = "Test Description";
+            byte[] testImage = new byte[64];
+            Dish testDish = new Dish(name, dishPrice, description, testImage, true, true, true, true, 15.2f);
+            Assert.AreEqual(testImage, testDish.Image);
+        }
+        [TestMethod]
+        public void DishModelKeepsETAValue()
+        {
+            string name = "TestName";
+            decimal dishPrice = 12.2m;
+            string description = "Test Description";
+            byte[] testImage = new byte[64];
+            float ETA = 15.2f;
+            Dish testDish = new Dish(name, dishPrice, description, testImage, true, true, true, true, ETA);
+            Assert.AreEqual(ETA, testDish.EstimatedTimeToComplete);
+        }
+
+    }
+}
---
+++ UnitTests/OrderModelTests.cs
@@ -0,0 +1,48 @@
+﻿using Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Models;
+using System.Windows.Forms;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class OrderModelTests
+    {
+        public Dish TestDish1 = new Dish("testName", 15, "testDescription", new byte[64], true, true, true, true, 15.0f);
+        public Dish TestDish2 = new Dish("testName2", 20, "testDescription2", new byte[64], true, false, true, false, 20.0f);
+        [TestMethod]
+        public void OrderModelKeepsDishListValue()
+        {
+
+            List<Dish> dishes = new List<Dish>();
+            dishes.Add(TestDish1);
+            dishes.Add(TestDish2);
+            Order order = new Order(dishes,200.0m,25.5f);
+            bool isEqual = (order.OrderedDishes.Split('-')[0] == TestDish1.Name) && (order.OrderedDishes.Split('-')[1] == TestDish2.Name);
+            Assert.IsTrue(isEqual);
+        }
+        [TestMethod]
+        public void OrderModelKeepsOrderPriceValue()
+        {
+            List<Dish> dishes = new List<Dish>();
+            dishes.Add(TestDish1);
+            dishes.Add(TestDish2);
+            Order order = new Order(dishes, 200.0m, 25.5f);
+            Assert.IsTrue(order.Price == 200.0m);
+        }
+        [TestMethod]
+        public void OrderModelKeepsOrderETAValue()
+        {
+            List<Dish> dishes = new List<Dish>();
+            dishes.Add(TestDish1);
+            dishes.Add(TestDish2);
+            Order order = new Order(dishes, 200.0m, 25.5f);
+            Assert.IsTrue(order.ETA == 25.5f);
+        }      
+    }
+}
