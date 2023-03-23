fad25b2044e9a08b4c01c23ca13c9dbb3e25817a
Kamui <Kamuicho>
2023-03-23 16:22:16 +0200

    Added UnitTests for the Order model.

--- UnitTests/OrderModelTests.cs
+++ UnitTests/OrderModelTests.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace UnitTests
 {
+    /// <summary>
+    /// A TestClass that contains UnitTests for the Order model.
+    /// </summary>
     [TestClass]
     public class OrderModelTests
     {
         public Dish TestDish1 = new Dish("testName", 15, "testDescription", new byte[64], true, true, true, true, 15.0f);
         public Dish TestDish2 = new Dish("testName2", 20, "testDescription2", new byte[64], true, false, true, false, 20.0f);
+        /// <summary>
+        /// A TestMethod that tests the OrderedDishes field in the Order model.
+        /// </summary>
         [TestMethod]
         public void OrderModelKeepsDishListValue()
         {
@@ -26,6 +32,9 @@ namespace UnitTests
             bool isEqual = (order.OrderedDishes.Split('-')[0] == TestDish1.Name) && (order.OrderedDishes.Split('-')[1] == TestDish2.Name);
             Assert.IsTrue(isEqual);
         }
+        /// <summary>
+        /// A TestMethod that tests the Price field in the Order model.
+        /// </summary>
         [TestMethod]
         public void OrderModelKeepsOrderPriceValue()
         {
@@ -35,6 +44,9 @@ namespace UnitTests
             Order order = new Order(dishes, 200.0m, 25.5f);
             Assert.IsTrue(order.Price == 200.0m);
         }
+        /// <summary>
+        /// A TestMethod that tests the ETA field in the Order model.
+        /// </summary>
         [TestMethod]
         public void OrderModelKeepsOrderETAValue()
         {
