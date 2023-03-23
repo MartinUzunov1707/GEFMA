ab0cce00ed7a2336f23f4323cf7bfb0e5692269b
Мартин Узунов <mu26969916@edu.mon.bg>
2023-03-23 16:02:02 +0200

    Added XML documentation to the RestaurantBusiness class.

--- Business/RestaurantBusiness.cs
+++ Business/RestaurantBusiness.cs
@@ -7,9 +7,16 @@ using Data;
 
 namespace Business
 {
+    /// <summary>
+    /// The RestaurantBusiness class handles all of the business logic for the RestaurantDB Database.
+    /// </summary>
     public class RestaurantBusiness
     {
         private RestaurantContext RestaurantContext;
+        /// <summary>
+        /// The GetAll Function returns the content of the Restaurant Database in the form of a List.
+        /// </summary>
+        /// <returns></returns>
         public List<Dish> GetAll()
         {
             using (RestaurantContext = new RestaurantContext())
@@ -17,6 +24,11 @@ namespace Business
                 return RestaurantContext.Dishes.ToList();
             }
         }
+        /// <summary>
+        /// The Get Function returns a Dish from the Restaurant Database by its ID.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Dish Get(int id)
         {
             using (RestaurantContext = new RestaurantContext())
@@ -24,6 +36,10 @@ namespace Business
                 return RestaurantContext.Dishes.Find(id);
             }
         }
+        /// <summary>
+        /// The Add function adds an item to the Restaurant Database.
+        /// </summary>
+        /// <param name="dish"></param>
         public void Add(Dish dish)
         {
             using (RestaurantContext = new RestaurantContext())
@@ -32,6 +48,10 @@ namespace Business
                 RestaurantContext.SaveChanges();
             }
         }
+        /// <summary>
+        /// The Update function updates a preexisting item from the Restaurant Database with new values.
+        /// </summary>
+        /// <param name="dish"></param>
         public void Update(Dish dish)
         {
             using (RestaurantContext = new RestaurantContext())
@@ -44,6 +64,10 @@ namespace Business
                 }
             }
         }
+        /// <summary>
+        /// The Delete function deletes an item from the Restaurant Database by its ID.
+        /// </summary>
+        /// <param name="id"></param>
         public void Delete(int id)
         {
             using (RestaurantContext = new RestaurantContext())
