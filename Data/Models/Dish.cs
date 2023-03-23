6ea2fe940fe98a09010a87c3791e5f7c2db95e2d
Kamui <Kamuicho>
2023-03-23 15:49:03 +0200

    Added XML documentation to Dish model.

--- Data/Models/Dish.cs
+++ Data/Models/Dish.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 namespace Data
 {
+    /// <summary>
+    /// Model for the Dish class. Database uses this model. (Code first approach)
+    /// </summary>
     public class Dish
     {
+        /// <summary>
+        /// Constructor for dish class. This constructor fills every field in the class.
+        /// </summary>
+        /// <param name="DishName"></param>
+        /// <param name="DishPrice"></param>
+        /// <param name="DishDesc"></param>
+        /// <param name="DishImage"></param>
+        /// <param name="DishIsVegetarian"></param>
+        /// <param name="DishIsVegan"></param>
+        /// <param name="DishIsGlutenFree"></param>
+        /// <param name="DishIsHalal"></param>
+        /// <param name="DishEstimatedTimeToComplete"></param>
         public Dish(string DishName, decimal DishPrice, string DishDesc, byte[] DishImage, bool DishIsVegetarian, bool DishIsVegan, bool DishIsGlutenFree, bool DishIsHalal, float DishEstimatedTimeToComplete)
         {
             Name = DishName;
@@ -20,8 +35,14 @@ namespace Data
             IsHalal = DishIsHalal;
             EstimatedTimeToComplete = DishEstimatedTimeToComplete;
         }
+        /// <summary>
+        /// Empty constructor for the class. Used in Testing.
+        /// </summary>
         public Dish()
         { }
+        /// <summary>
+        /// Public fields that the class and database contain.
+        /// </summary>
         public int ID { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
